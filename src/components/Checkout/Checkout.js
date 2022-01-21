5b0c56fe29e880fe5c398300b26d1cfad392a95b
Fran Leg <francolegnazzi@hotmail.com>
2022-01-21 12:47:37 -0300

    Entrega Proyecto Final Coderhouse

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,36 +1,43 @@
-import { useState } from "react";
 import { useCartContext } from "../../context/CartContext";
-import Validate from "../../helpers/Validate/Validate.js"
 import { Navigate } from 'react-router';
-import Form from "../Form/Form"
+import { Formik } from "formik";
 import { generateOrder } from "../../config/generateOrder.js"
+import * as Yup from "yup"
 import "./Checkout.css";
+
+const schema = Yup.object().shape({
+    name: Yup.string()
+            .required("Este campo es obligatorio")
+            .min(3, "Nombre inválido")
+            .max(30, "Nombre inválido"),
+    lastName: Yup.string()
+            .required("Este campo es obligatorio")
+            .min(3, "Nombre inválido")
+            .max(30, "Nombre inválido"),
+    phone: Yup.string()
+            .required("Este campo es obligatorio")
+            .min(8, "Teléfono inválido"),           
+    email: Yup.string()
+            .required("Este campo es obligatorio")
+            .email("Email inválido")
+            .oneOf([Yup.ref("emailConfirm")], "Los emails no coinciden"),
+    emailConfirm: Yup.string()
+            .required("Este campo es obligatorio")
+            .email("Email inválido") 
+            .oneOf([Yup.ref("email")], "Los emails no coinciden") 
+            
+})
 
 function Checkout() { 
     const {cartList, totalPurchase, emptyCart} = useCartContext();
 
-    const [values, setValues] = useState({
+    const initialValues = {
         name: "",
         lastName: "",
         phone: "",
         email: "",
         emailConfirm: "",
-    });
-
-    const handleInputChange = (e) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if (!Validate(values)) {return}
-        
-        generateOrder(values, cartList, totalPurchase, emptyCart)
-    }
+    };
 
     return (
         <>
@@ -46,7 +53,79 @@ function Checkout() {
                     </div>
                     <div className="row">
                         <div className="col-lg-12 col-xs-12 form-container">
-                            <Form handleSubmit={handleSubmit} handleInputChange={handleInputChange} values={values}/>
+                        <Formik
+                            initialValues={initialValues}
+                            validationSchema={schema}
+                            onSubmit={(values)=>{
+                                generateOrder(values, cartList, totalPurchase, emptyCart)
+                            }}
+                        >
+                            {(formik) => (
+                                <form onSubmit={formik.handleSubmit}>
+                                <label className="title-form">Nombre</label>
+                                <input 
+                                    onChange={formik.handleChange} 
+                                    name= "name"
+                                    value= {formik.values.name}
+                                    className= "form-control my-2"
+                                    type= "text"
+                                    placeholder= "Escriba su nombre"
+                                />
+                                {formik.errors.name && <small>{formik.errors.name}</small>}
+                                <br/>
+
+                                <label className="title-form">Apellido</label>
+                                <input 
+                                    onChange={formik.handleChange} 
+                                    name= "lastName"
+                                    value= {formik.values.lastName}
+                                    className="form-control my-2"
+                                    type= "text"
+                                    placeholder= "Escriba su apellido"
+                                />
+                                {formik.errors.lastName && <small>{formik.errors.lastName}</small>}
+                                <br/>
+
+                                <label className="title-form">Telefono</label>
+                                <input 
+                                    onChange={formik.handleChange} 
+                                    name= "phone"
+                                    value= {formik.values.phone}
+                                    className="form-control my-2"
+                                    type= "number"
+                                    placeholder= "Escriba su teléfono"
+                                />
+                                {formik.errors.phone && <small>{formik.errors.phone}</small>}
+                                <br/>
+
+                                <label className="title-form">E-mail</label>
+                                <input 
+                                    onChange={formik.handleChange} 
+                                    name= "email"
+                                    value= {formik.values.email}
+                                    className="form-control my-2"
+                                    type= "email"
+                                    placeholder= "nombre@ejemplo.com"
+                                /> 
+                                {formik.errors.email && <small>{formik.errors.email}</small>}
+                                <br/>
+
+                                <label className="title-form">E-mail</label>
+                                <input 
+                                    onChange={formik.handleChange} 
+                                    name= "emailConfirm"
+                                    value= {formik.values.emailConfirm}
+                                    className="form-control my-2"
+                                    type= "email"
+                                    placeholder= "Repita su e-mail"
+                                /> 
+                                {formik.errors.emailConfirm && <small>{formik.errors.emailConfirm}</small>}                               
+                                <div className="text-center">    
+                                    <button type="submit" className="btn-confirmPurchase">Confirmar Compra</button>    
+                                </div>                             
+                            </form>
+                            )} 
+                        </Formik>
                         </div>
                     </div>
                 </div>
@@ -56,6 +135,4 @@ function Checkout() {
 }
 
 export default Checkout
-
-
 
